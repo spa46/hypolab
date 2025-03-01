87a9c58390549df9c7e61087136abc0d38a532bf
seanpark <psckyky@hotmail.com>
2025-03-01 11:31:04 +0900

    [FIX] database table name

--- hypolab/hypolab/settings.py
+++ hypolab/hypolab/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'hypolab.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'HypoDB',
+        'NAME': 'hypodb',
         'USER': 'test',
         'PASSWORD': 'test1234',
         'HOST': 'localhost',  # Set to 'localhost' or the IP address of your database server
