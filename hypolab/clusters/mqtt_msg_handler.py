2eb8a2bf6d9dbd5eb4c5f7e60418eec59da92dba
seanpark <psckyky@hotmail.com>
2025-03-09 18:34:19 +0900

    [prototype] preparation for managing mqtt

--- hypolab/clusters/mqtt_msg_handler.py
+++ hypolab/clusters/mqtt_msg_handler.py
@@ -1,18 +1,40 @@
-# mqtt_msghandler.py
-from .models import HypoCluster
-
-
 def mqtt_message(topic, payload):
-    topic_parts = topic.split('/')
+    from .models import HypoCluster  # Move the import inside the function
 
-    if len(topic_parts) == 3 and topic_parts[2] == 'status':
+    topic_parts = topic.split('/')
+    if len(topic_parts) == 3:
         cluster_id = topic_parts[1]
-        try:
-            cluster = HypoCluster.objects.get(id=cluster_id)
-            if payload == 'connected':
-                cluster.is_connected = True
-            elif payload == 'disconnected':
-                cluster.is_connected = False
-            cluster.save()
-        except HypoCluster.DoesNotExist:
-            print(f'Cluster with id {cluster_id} does not exist')
+        if topic_parts[2] == 'status':
+            handle_status_message(cluster_id, payload)
+        elif topic_parts[2] == 'control':
+            handle_control_message(cluster_id, payload)
+
+def handle_status_message(cluster_id, payload):
+    from .models import HypoCluster  # Import inside the function
+
+    try:
+        cluster = HypoCluster.objects.get(id=cluster_id)
+        if payload == 'connected':
+            cluster.is_connected = True
+        elif payload == 'disconnected':
+            cluster.is_connected = False
+        cluster.save()
+
+        # Send WebSocket message
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            "cluster_status",
+            {
+                "type": "cluster_status_update",
+                "message": {
+                    "id": cluster_id,
+                    "is_connected": cluster.is_connected,
+                },
+            },
+        )
+    except HypoCluster.DoesNotExist:
+        print(f'Cluster with id {cluster_id} does not exist')
+
+def handle_control_message(cluster_id, payload):
+    # Handle control messages
+    print(f'Control message received for cluster {cluster_id}: {payload}')
